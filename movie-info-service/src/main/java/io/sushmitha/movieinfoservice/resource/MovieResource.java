9240bade2278aec6224314e7147a3872d61e6c16
mehersushu <patnamsushmithameher@gmail.com>
2024-08-04 23:08:43 -0500

    Info

--- movie-info-service/src/main/java/io/sushmitha/movieinfoservice/resource/MovieResource.java
+++ movie-info-service/src/main/java/io/sushmitha/movieinfoservice/resource/MovieResource.java
@@ -4,13 +4,13 @@ import io.sushmitha.movieinfoservice.model.Movie;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+//test
 @RestController
 @RequestMapping("/movies")
 public class MovieResource {
     @RequestMapping("/{movieId}")
     public Movie getInfo(@PathVariable("movieId") String movieId) {
 
-        return new Movie("Thor",movieId);
+        return new Movie("Thor 123",movieId);
     }
 }
